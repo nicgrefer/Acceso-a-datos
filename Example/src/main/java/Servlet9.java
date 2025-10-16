6a8905d10eb7e8374b2acc1259671b90167aada1
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-10-16 13:20:10 +0200

    Add Servlet9 to track user visits with cookies
    Introduces Servlet9, which counts the number of user visits using a cookie named 'cont'. The servlet increments the visit count on each GET request and sets the cookie to expire in one year.

---
+++ Example/src/main/java/Servlet9.java
@@ -0,0 +1,59 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Servlet9
+ */
+@WebServlet("/Servlet9")
+public class Servlet9 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet9() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// la primera vez no va a encontrar la cocky y se crea. Despues ya existira y lo encontrara
+		boolean bandera = false;
+		int n = 0;
+		
+		response.setContentType("text/plain");
+		// Recuperamos el vector de cookis
+		Cookie[] cs = request.getCookies();
+		
+		// Forma normal para recojer la cooky
+		if (cs != null) {
+			for (int i = 0; i<cs.length && !bandera; i++) {
+				// Busco la cooki "cont" en el vector
+				if (cs[i].getName().equals("cont")) {
+					n=Integer.parseInt(cs[i].getValue())+1;
+					bandera = true;
+				}
+			}
+		}
+		// Creamos la cookie
+		Cookie c = new Cookie("cont", String.valueOf(n));
+		c.setMaxAge(60*60*365);// caduca al cabo del año
+		// Se envia la coky como respuesta al navegador
+		response.addCookie(c);
+		response.getWriter().append("Numero de visitas: " + n);
+		response.getWriter().close();
+		
+	}
+
+}
