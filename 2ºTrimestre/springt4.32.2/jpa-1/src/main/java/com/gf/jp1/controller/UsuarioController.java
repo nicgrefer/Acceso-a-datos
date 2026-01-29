f046299e28ccb1a76f9bc323dc446f0f153b8e66
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2026-01-29 13:45:32 +0100

    ajado mas funciones
    insert, update, delete

--- 2ºTrimestre/springt4.32.2/jpa-1/src/main/java/com/gf/jp1/controller/UsuarioController.java
+++ 2ºTrimestre/springt4.32.2/jpa-1/src/main/java/com/gf/jp1/controller/UsuarioController.java
@@ -3,11 +3,18 @@ package com.gf.jp1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gf.jp1.model.Usuario;
 import com.gf.jp1.service.UsuarioService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class UsuarioController {
@@ -19,4 +26,22 @@ public class UsuarioController {
 	public List<Usuario> getAll(){
 		return (List<Usuario>) usuarios.findAll();
 	}
+	
+	@PostMapping("/insert")
+	public void createUser(@RequestBody Usuario user) {
+        usuarios.save(user);
+    }
+	
+	@PutMapping("/update")
+	public void updateUser(@RequestBody Usuario user) {
+		usuarios.save(user);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteUser(@PathVariable Long id) {
+	    usuarios.delete(id);
+	}
+
+	
+	
 }
